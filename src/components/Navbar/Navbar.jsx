5d5f8f896c42c532cdcf3ea4e4927c9d6bc73730
shu-william <williamshu@outlook.com>
2024-06-28 17:35:58 -0700

    added navbar icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import './Navbar.scss';
 import LogoS from '../../assets/images/logo-s.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Navbar = () => {
   return (
@@ -9,6 +12,24 @@ const Navbar = () => {
       <Link className="logo" to="/">
         <img src={LogoS} alt="logo" />
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} color="4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/williamshu8/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
